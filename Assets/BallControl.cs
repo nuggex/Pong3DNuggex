cfe34bf430266fe0470f9575f7e8ef905b51f96e
nuggex <nuggex@gmail.com>
2020-11-04 10:56:44 +0200

    Fixed ball velocity
    Add velocity on paddle hit, and paddle material. No effect from walls. Limited bounces on either side to 2 to win.

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class BallControl : MonoBehaviour
 {
     Rigidbody ballRB;
-
+    int bouncesPlayer;
+    int bouncesComputer;
     private void Start()
     {
         GameManager.instance.currentBall = gameObject;
@@ -14,24 +15,59 @@ public class BallControl : MonoBehaviour
 
     private void Update()
     {
-        if(ballRB.velocity.magnitude > 400 || transform.position.y > 51 || transform.position.y < -1 || transform.position.x > 51 ||transform.position.x < -51 ||transform.position.z > 55 ||transform.position.z < -55)
+        /*Debug.Log("BallRB Transform" + ballRB.transform.position);
+        Debug.Log("BallRB Velocity" + ballRB.velocity);
+        Debug.Log("BallRB Velocity" + ballRB.velocity.magnitude);
+        */
+        if (bouncesComputer > 2)
+        {
+            GameManager.instance.playerScore += 1;
+            GameManager.instance.ReturnAwardPC(1.0f);
+            Destroy(gameObject);
+        }
+        if (bouncesPlayer > 2)
+        {
+            GameManager.instance.computerScore += +1;
+            GameManager.instance.ReturnAwardPC(-1.0f);
+            Destroy(gameObject);
+        }
+
+        if (transform.position.y > 51 || transform.position.y < -1 || transform.position.x > 51 || transform.position.x < -51 || transform.position.z > 55 || transform.position.z < -55)
         {
             Destroy(gameObject);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        /*if(collision.collider.tag == "paddle")
+        if (collision.collider.tag == "paddle")
         {
-            ballRB.AddForce(-ballRB.velocity, ForceMode.Impulse);
-        }*/
-        if(collision.collider.tag == "wall")
-        {
-            ballRB.AddForce(ballRB.velocity * 0.5f, ForceMode.Impulse);
+            bouncesPlayer = 0;
+            bouncesComputer = 0;
         }
-        if(collision.collider.tag == "floor")
+        if (collision.collider.tag == "westwall")
         {
-            ballRB.AddForce(ballRB.velocity * 0.5f, ForceMode.Impulse);
+            //ballRB.velocity = ballRB.velocity * 1.5f;
+            // ballRB.AddForce(ballRB.velocity, ForceMode.Impulse);
+        }
+        if (collision.collider.tag == "eastwall")
+        {
+            //ballRB.velocity = ballRB.velocity * 1.5f;
+            //ballRB.AddForce(ballRB.transform.position, ForceMode.Impulse);
+            //ballRB.AddForce(ballRB.velocity, ForceMode.Impulse);
+        }
+        if (collision.collider.tag == "floor")
+        {
+            if (ballRB.transform.position.z < 0)
+            {
+                bouncesComputer += 1;
+            }
+            else if (ballRB.transform.position.z >= 0)
+            {
+                bouncesPlayer += 1;
+            }
+            //ballRB.velocity = ballRB.velocity * 1.5f;
+            //ballRB.AddForce(ballRB.transform.position, ForceMode.Impulse);
+            //ballRB.AddForce(ballRB.velocity, ForceMode.Impulse);
 
         }
         if (collision.collider.name == "GoalP1")
@@ -41,7 +77,7 @@ public class BallControl : MonoBehaviour
         }
         if (collision.collider.name == "GoalP2")
         {
-            GameManager.instance.ReturnAwardPC(1.0f); 
+            GameManager.instance.ReturnAwardPC(1.0f);
             GameManager.instance.playerScore += (int)Score.goal;
         }
 
@@ -49,7 +85,15 @@ public class BallControl : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        
 
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "paddle")
+        {
+            ballRB.AddForce(ballRB.velocity, ForceMode.Impulse);
+        }
     }
 }
